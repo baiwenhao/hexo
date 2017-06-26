bf288773211193c3b2c2554e0a3fd8d110a03cf2
baiwenhao.sh <bwh2009@qq.com>
2017-06-26 18:36:03 +0800

    add

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 var log = (str) => console.log(str)
-const obj = {values:{}}
-const d = new Date()
-let i = 1
-for (; i < 30; i++) {
-  if (i === 1) {
-    obj.defaultValue = d.toLocaleDateString()
-  }
-  obj.values[i]= d.toLocaleDateString()
+var toString = Object.prototype.toString;
+function isArray(val) {
+  return toString.call(val) === '[object Array]'
 }
-obj.style = 'center'
+function forEach (obj, fn) {
+  if (obj === null || typeof obj === 'undefined') return
+  if (typeof obj !== 'object' && !isArray(obj)) obj = [obj]
+  if (isArray(obj)) {
+    for (var i = 0, l = obj.length; i < l; i++) {
+      fn.call(null, obj[i], i, obj)
+    }
+  } else {
+    for (var key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        fn.call(null, obj[key], key, obj)
+      }
+    }
+  }
+}
 
-log(d.toLocaleDateString())
+function merge(/* obj1, obj2, obj3, ... */) {
+  var result = {}
+  function assignValue(val, key) {
+    if (typeof result[key] === 'object' && typeof val === 'object') {
+      result[key] = merge(result[key], val)
+    } else {
+      result[key] = val
+    }
+  }
+  for (var i = 0, l = arguments.length; i < l; i++) {
+    forEach(arguments[i], assignValue)
+  }
+  return result
+}
+
+var a = [1,2,3,4,5]
+var b = {a: 1,b: 2,c: 4, e: {
+  aa: 10,
+  b: 11
+}}
+
+var m = merge(a, b)
+
+console.log(m)
 
---
+++ source/_posts/axios.md
@@ -0,0 +1,11 @@
+---
+title: Axios源码
+abbrlink: a09486d2
+date: 2017-06-23 10:12:01
+tags:
+---
+
+
+
+
+
